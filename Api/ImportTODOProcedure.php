850a564f6c06d5f190c2f2115c07a8fbfabff7e2
Taras Kushnir <kushnirTV@gmail.com>
2019-08-18 17:16:19 -0700

    Update tasks

--- Api/ImportTODOProcedure.php
+++ Api/ImportTODOProcedure.php
@@ -2,7 +2,9 @@
 
 namespace Kanboard\Plugin\TdgImport\Api;
 
+use Kanboard\Api\Authorization\ProjectAuthorization;
 use Kanboard\Api\Procedure\BaseProcedure;
+use Kanboard\Model\CategoryModel;
 use Kanboard\Core\Base;
 
 /**
@@ -12,34 +14,116 @@ use Kanboard\Core\Base;
  */
 class ImportTODOProcedure extends BaseProcedure
 {
-	public function importTodoComments($root, $branch, $author, $project, array $comments)
-	{
-       ///ProjectAuthorization::getInstance($this->container)->check($this->getClassName(), 'importTodoComments', $todoTasks);
+    public function importTodoComments($root, $branch, $author, $project, array $comments) {
+        $project_row = $this->projectModel->getByName($project);
+        if (!$project_row) { return false; }
+        $project_id = $project_row['id'];
 
-        $project_id = $this->projectModel->getByName($project)['id'];
+        ProjectAuthorization::getInstance($this->container)->check($this->getClassName(), 'importTodoComments', $project_id);
 
-		foreach ($comments as $c) {
-			$values = array(
-				'title' => $c['title'],
-                    'project_id' => $project_id,
-                    //'color_id' => $color_id,
-                    //'column_id' => $column_id,
-                    //'owner_id' => $owner_id,
-                    //'creator_id' => $creator_id,
-                    'description' => $c['body'],
-                    //'category_id' => $category_id,
-                    //'reference' => $reference,
-                    //'tags' => $tags,
-                    //'date_started' => $date_started,
-                );
+        $categoryToIDMap = $this->createCategoriesMap($project_id, $comments);
+        $existingTasks = $this->createExistingTasksMap($project_id);
+        $inputTasks = $this->createInputTaskMap($comments);
 
-			list($valid, ) = $this->taskValidator->validateCreation($values);
+        // add new or update existing tasks
+        foreach ($inputTasks as $hash => $c) {
+            $task_id = null;
+            if (array_key_exists($hash, $existingTasks)) {
+                $task_id = $existingTasks[$hash]['id'];
+            }
+            $this->addTodoComment($c, $project_id, $branch, $categoryToIDMap, $task_id);
+        }
+        return true;
+    }
 
-			if ($valid) {
-				$this->taskCreationModel->create($values);
-			}
-		}
-		return true;
-        //return $this->taskApiFormatter->withTask($task)->format();
-	}
+    private function addTodoComment($c, $project_id, $branch, $categoryToIDMap, $task_id=null) {
+        $reference = $c['file'] . ":" . $c['line'];
+
+        $values = array(
+            'title' => $c['title'],
+            'project_id' => $project_id,
+            //'color_id' => $color_id,
+            //'column_id' => $column_id,
+            //'owner_id' => $owner_id,
+            //'creator_id' => $creator_id,
+            'description' => $c['body'],
+            'reference' => $reference,
+        );
+
+        if ($task_id) {
+            $values['id'] = $task_id;
+        }
+
+        $shouldCreate = empty($task_id);
+
+        if ($branch && $shouldCreate) {
+            $tags = array("@" . $branch);
+            $values['tags'] = $tags;
+        }
+
+        $category = $c['category'];
+        if ($category && array_key_exists($category, $categoryToIDMap)) {
+            $category_id = $categoryToIDMap[$category];
+            $values['category_id'] = $category_id;
+        }
+
+        list($valid, ) = $this->taskValidator->validateCreation($values);
+
+        if ($valid) {
+            if ($shouldCreate) {
+                $this->taskCreationModel->create($values);
+            } else {
+                $this->taskModificationModel->update($values);
+            }
+        }
+    }
+
+    private function createCategoriesMap($project_id, $comments) {
+        $categories = array();
+        foreach ($comments as $c) {
+            if (array_key_exists('category', $c)) { 
+                $categories[] = $c['category']; 
+            }
+        }
+
+        $existing_categories = $this->categoryModel->getList($project_id, false /*prepend none*/, false /*prepend all*/);
+        $categoryToIDMap = array();
+        foreach ($existing_categories as $key => $value) {
+            $categoryToIDMap[$value] = $key;
+        }
+
+        foreach ($categories as $category) {
+            if (!array_key_exists($category, $categoryToIDMap)) {
+                $id = $this->categoryModel->create(
+                    array(
+                        'project_id' => $project_id,
+                        'name' => $category,
+                    ));
+                if ($id) {
+                    $categoryToIDMap[$category] = $id;
+                }
+            }
+        }
+
+        return $categoryToIDMap;
+    }
+
+    private function createExistingTasksMap($project_id) {
+        $taskMap = array();
+        $tasks = $this->taskFinderModel->getAll($project_id);
+        foreach ($tasks as $t) {
+            $hash = md5($t['title'] . $t['description']);
+            $taskMap[$hash] = $t;
+        }
+        return $taskMap;
+    }
+
+    private function createInputTaskMap($comments) {
+        $taskMap = array();
+        foreach ($comments as $c) {
+            $hash = md5($c['title'] . $c['body']);
+            $taskMap[$hash] = $c;
+        }
+        return $taskMap;    
+    }
 }
